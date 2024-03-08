094584d988d400828db78eb4ee4ba34e913c7f60
Samuel B. Oldra <samuel.oldra@gmail.com>
2024-03-08 00:00:00 -0300

    Configurado e adicionados comentários XML para o Swagger #4

--- Agenda.API/Program.cs
+++ Agenda.API/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +9,27 @@ builder.Services.AddControllers();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(o =>
+{
+    o.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Title = "Agenda.API",
+            Version = "v1",
+            Contact = new OpenApiContact
+            {
+                Name = "Samuel B. Oldra",
+                Email = "samuel.oldra@gmail.com",
+                Url = new Uri("https://github.com/samuel-oldra")
+            }
+        }
+    );
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    o.IncludeXmlComments(xmlPath);
+});
 
 // Serilog
 builder
