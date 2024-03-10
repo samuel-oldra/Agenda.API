afb45be11bc0f9435ea397d9b79a816ea1682ce2
Samuel B. Oldra <samuel.oldra@gmail.com>
2024-03-10 00:00:00 -0300

    Validação de dados com DataAnnotations (RegularExpression) #6

--- Agenda.API/Models/ContatoPutInputModel.cs
+++ Agenda.API/Models/ContatoPutInputModel.cs
@@ -1,18 +1,21 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using Agenda.API.Utils;
 
 namespace Agenda.API.Models
 {
     public class ContatoPutInputModel
     {
         [Required(ErrorMessage = "Esse campo é obrigatório")]
-        [MinLength(3, ErrorMessage = "Esse campo deve conter entre 3 e 100 caracteres")]
-        [MaxLength(100, ErrorMessage = "Esse campo deve conter entre 3 e 100 caracteres")]
+        [MinLength(10, ErrorMessage = "Esse campo deve conter entre 10 e 100 caracteres")]
+        [MaxLength(100, ErrorMessage = "Esse campo deve conter entre 10 e 100 caracteres")]
+        [RegularExpression(ExpressoesRegulares.EMAIL, ErrorMessage = "E-mail inválido")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Esse campo é obrigatório")]
-        [MinLength(3, ErrorMessage = "Esse campo deve conter entre 3 e 50 caracteres")]
-        [MaxLength(50, ErrorMessage = "Esse campo deve conter entre 3 e 50 caracteres")]
+        [MinLength(8, ErrorMessage = "Esse campo deve conter entre 8 e 50 caracteres")]
+        [MaxLength(50, ErrorMessage = "Esse campo deve conter entre 8 e 50 caracteres")]
+        [RegularExpression(ExpressoesRegulares.TELEFONE, ErrorMessage = "Telefone inválido")]
         public string Telefone { get; set; }
 
         public DateTime DataNascimento { get; set; }
